d4b7116f7680f5efc48fb6f835bceea959f66f6d
HarshKakran <harshkakran05@gmail.com>
2023-01-20 18:00:32 +0530

    Added user link in models

--- backend/resume_builder/rb/models.py
+++ backend/resume_builder/rb/models.py
@@ -1,9 +1,11 @@
 import datetime
 from django.db import models
 from django.contrib.postgres.fields import ArrayField
+from django.contrib.auth.models import User
 
 class Resume(models.Model):
 
+    user = models.ForeignKey('User', on_delete=models.CASCADE)
     f_name = models.CharField(max_length=45)
     l_name = models.CharField(max_length=45)
 
@@ -35,6 +37,7 @@ class Contact(models.Model):
         (i, i) for i in [PLATFORM_PH, PLATFORM_EMAIL, PLATFORM_LINKEDIN, PLATFORM_GITHUB])
 
 
+    user = models.ForeignKey('User', on_delete=models.CASCADE)
     platform = models.CharField(
         max_length=128, null=True, blank=True, choices=PLATFORM_CHOICES)
     url = models.CharField(max_length=512)
@@ -44,6 +47,7 @@ class Education(models.Model):
         (i, i) for i in range(1980, datetime.datetime.now().year+1)
     )
 
+    user = models.ForeignKey('User', on_delete=models.CASCADE)
     programme = models.CharField(max_length=255)
     institute = models.CharField(max_length=255)
     s_yr = models.IntegerField(choices=YEAR_CHOICES, default=datetime.datetime.now().year)
@@ -55,6 +59,7 @@ class WorkX(models.Model):
         (i, i) for i in range(1980, datetime.datetime.now().year + 1)
     )
 
+    user = models.ForeignKey('User', on_delete=models.CASCADE)
     role = models.CharField(max_length=512)
     org = models.CharField(max_length=512)
     desc = models.TextField(null=True, blank=True)
@@ -62,6 +67,7 @@ class WorkX(models.Model):
     e_yr = models.IntegerField(choices=YEAR_CHOICES, default=datetime.datetime.now().year)
 
 class Project(models.Model):
+    user = models.ForeignKey('User', on_delete=models.CASCADE)
     name = models.CharField(max_length=255, null=False, blank=False)
     desc = models.TextField(null=True, blank=True)
     url = models.CharField(max_length=512)
